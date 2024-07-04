77219f874143b49c534efdcf4c7104a40f174aba
eurojeff <eurojeff8@gmail.com>
2024-07-04 22:54:02 +0200

    Letters

---
+++ LearningProjects/bootcamp/finky_code.py
@@ -0,0 +1,36 @@
+
+char = "#"
+#print the letter C
+
+print(f" {char*4}")
+print(f"{char} ")
+print(f"{char} ")
+print(f"{char} ")
+print(f" {char*4}")
+print("")
+
+#print the letter O
+print(f" {char*4}")
+print(f"{char}   {char}")
+print(f"{char}   {char}")
+print(f"{char}   {char}")
+print(f" {char*4}")
+print("")
+
+
+#print the letter D
+print(f"{char*3} ")
+print(f"{char}  {char}")
+print(f"{char}  {char}")
+print(f"{char}  {char}")
+print(f"{char*3} ")
+print("")
+
+
+#print the letter E
+print(f"{char*4}")
+print(f"{char}")
+print(f"{char*4}")
+print(f"{char}")
+print(f"{char*4}")
+print("")

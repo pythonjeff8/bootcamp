845555654a94904206acd77a5ccf6ab93abd68c2
eurojeff <eurojeff8@gmail.com>
2024-07-07 10:18:44 +0200

    CompoundUpdate

--- LearningProjects/bootcamp/compound_datatypes.py
+++ LearningProjects/bootcamp/compound_datatypes.py
@@ -46,3 +46,28 @@ while x < res:
     print(books[x]["author"])
     print(books[x]["title"])
     x = x + 1
+
+
+courses = [{"Name":"Math", "Student1":"JK Rowling", "Student2":"John Jones"},
+         
+         {"Name":"History", "Student1":"Mary Jane", "Student2":"George Jamison", "Student3":"Mary Mary"},
+         
+         {"Name":"Chemistry", "Student1":"Jane Horsefeathers", "Student2":"James John"}]
+
+courses[0]["Student3"] = "Jimmy Johnson"
+courses[0]["Student4"] = "Janey Johns"
+courses[0]["Student5"] = "Joan Howard"
+courses[0]["Student6"] = "Bobby Billings"
+courses[0]["Student7"] = "Jim Johanson"
+
+print(courses)
+
+del courses[1]["Student2"]
+
+print(courses)
+
+print(courses[2])
+
+courses.append({"Name":"Physics", "Student1":"George Jones", "Student2":"Fred Flintstone", "Student3":"Betty Rubble", "Student4":"Barney Dino"})
+
+print(courses[3])

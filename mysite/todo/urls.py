da5d62ac64ed01cd510972eb77f9f1746b80e2b2
eurojeff <eurojeff8@gmail.com>
2024-08-23 18:13:55 +0200

    Webapp updates

--- mysite/todo/urls.py
+++ mysite/todo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import AboutView, index
 
 urlpatterns = [
     path('', index, name='index'),
+    #path("about/", about, name = "about"),
+    path("about/", AboutView.as_view(), name = "about"),
+    
     ]
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,6 +1,18 @@
+#from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("<h1>Todo!</h1>")
+
+#     Function methond vs class object method
+# def about(request):
+#    return HttpResponse("This is the about page")
+
+class AboutView(View):
+    def get(self, request):
+        return HttpResponse("This is the about page")
+    
